750c6b41cb46330f4767f3d012a5fcfffdf4ff46
kilian-ai <kilian@voneu.co>
2026-04-10 10:58:16 -0700

    bump ASSET_REV to 1fe6ffe (side-table QuickJS allocator)

--- traits/www/linux/linux.rs
+++ traits/www/linux/linux.rs
@@ -212,7 +212,7 @@ const BOOT_SCRIPT: &str = r#"
 
 (async function bootLinuxWasm() {
     const CDN = 'https://kilian-ai.github.io/linux-wasm';
-    const ASSET_REV = '5baa761';
+    const ASSET_REV = '1fe6ffe';
     const assetUrl = (name) => `${CDN}/${name}?rev=${ASSET_REV}`;
     const COI_SW_RELOAD_KEY = 'linux-wasm-coi-v1';
 
