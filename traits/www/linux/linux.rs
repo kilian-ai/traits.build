46d5cca663eb33147f6a11116247c078ad04b288
kilian-ai <kilian@voneu.co>
2026-04-10 20:17:29 -0700

    linux: bump ASSET_REV to d7735bc (add git v2.47.2 to guest)

--- traits/www/linux/linux.rs
+++ traits/www/linux/linux.rs
@@ -219,7 +219,7 @@ const BOOT_SCRIPT: &str = r#"
 
 (async function bootLinuxWasm() {
     const CDN = 'https://kilian-ai.github.io/linux-wasm';
-    const ASSET_REV = 'e341fff';
+    const ASSET_REV = 'd7735bc';
     const assetUrl = (name) => `${CDN}/${name}?rev=${ASSET_REV}`;
     const COI_SW_RELOAD_KEY = 'linux-wasm-coi-v1';
 
